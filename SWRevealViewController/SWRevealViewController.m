f7dd2f2cbd5f36827d5e28eae008ba59d9925025
Joel Reis <joel@testfire.io>
2015-02-19 16:17:05 -0500

    Added requried targetTransform method Context Transition Object.

--- SWRevealViewController/SWRevealViewController.m
+++ SWRevealViewController/SWRevealViewController.m
@@ -446,6 +446,10 @@ static CGFloat scaledValue( CGFloat v1, CGFloat min2, CGFloat max2, CGFloat min1
     return nil;
 }
 
+- (CGAffineTransform)targetTransform
+{
+    return CGAffineTransformMakeRotation(0);
+}
 
 - (CGRect)initialFrameForViewController:(UIViewController *)vc
 {
