06153dd35eafe5494d5305f91e62d2e1f5472de5
Joel Reis <joel@testfire.io>
2015-02-19 15:02:22 -0500

    Made MenuVC's cells static to remove Xcode warning.

--- RevealControllerStoryboardExample2/RevealControllerStoryboardExample2/MenuViewController.m
+++ RevealControllerStoryboardExample2/RevealControllerStoryboardExample2/MenuViewController.m
@@ -31,43 +31,6 @@
     }
 }
 
-
-#pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    return 1;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return 3;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *CellIdentifier = @"Cell";
-
-    switch ( indexPath.row )
-    {
-        case 0:
-            CellIdentifier = @"map";
-            break;
-            
-        case 1:
-            CellIdentifier = @"blue";
-            break;
-
-        case 2:
-            CellIdentifier = @"red";
-            break;
-    }
-
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: CellIdentifier forIndexPath: indexPath];
- 
-    return cell;
-}
-
 #pragma mark state preservation / restoration
 - (void)encodeRestorableStateWithCoder:(NSCoder *)coder {
     NSLog(@"%s", __PRETTY_FUNCTION__);
